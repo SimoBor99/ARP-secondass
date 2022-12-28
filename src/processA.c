cd4cf56d532fcfec1a20af77ca15d194dc4ae380
Matteoforni1 <93779567+Matteoforni1@users.noreply.github.com>
2022-12-28 16:51:05 +0100

    Aggiunta funzionalità snaphot (da modificare dopo che implementiamo spostamento cerchio)

--- src/processA.c
+++ src/processA.c
@@ -1,5 +1,41 @@
+// we include libraries
 #include "./../include/processA_utilities.h"
+#include <stdio.h>
+#include <bmpfile.h>
+#include <stdlib.h>
+#include <math.h>
 
+int n_snapshot = 0;
+
+// function to be executed when the 'P' button is pressed
+void print_circle() {
+	bmpfile_t *bmp;
+  	rgb_pixel_t pixel = {255, 0, 0, 0};
+  
+  	int width = 100;
+  	int height = width;
+  	int depth = 4;
+  	char path[100] = "out/snap";
+  	char num[20];
+	bmp = bmp_create(width, height, depth);
+  
+	int radius = 30;
+	for(int x = -radius; x <= radius; x++) {
+		for(int y = -radius; y <= radius; y++) {
+	      		if(sqrt(x*x + y*y) < radius) {
+          			bmp_set_pixel(bmp, width/2 + x, height/2 + y, pixel);
+      			}
+    		}
+  	}
+  	sprintf(num,"%d",n_snapshot);
+  	strcat(path,num);
+  	strcat(path,".bmp");
+  	bmp_save(bmp, path);
+  	n_snapshot += 1;
+  	bmp_destroy(bmp);
+}
+
+// main function
 int main(int argc, char *argv[])
 {
     // Utility variable to avoid trigger resize event on launch
@@ -30,6 +66,7 @@ int main(int argc, char *argv[])
                 if(check_button_pressed(print_btn, &event)) {
                     mvprintw(LINES - 1, 1, "Print button pressed");
                     refresh();
+                    print_circle();
                     sleep(1);
                     for(int j = 0; j < COLS - BTN_SIZE_X - 2; j++) {
                         mvaddch(LINES - 1, j, ' ');
