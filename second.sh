6c3866461df05deaa3d242d4a4664b31101c5f90
Matteoforni1 <93779567+Matteoforni1@users.noreply.github.com>
2022-12-28 16:47:15 +0100

    Aggiunte flag necessarie per la corretta compilazione

--- second.sh
+++ second.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-gcc src/master.c -o bin/master -lrt
-gcc src/processA.c -lncurses -o bin/processA -lrt -pthread
-gcc src/processB.c -lncurses -o bin/processB -lrt -pthread
+gcc src/master.c -o bin/master -lrt -lm
+gcc src/processA.c -lncurses -o bin/processA -lbmp -lrt -pthread -lm
+gcc src/processB.c -lncurses -o bin/processB -lrt -pthread -lm
 ./bin/master
