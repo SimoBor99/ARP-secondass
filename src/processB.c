e666b2f94c4eeb6bccb2027b330ca7c162844432
Matteoforni1 <93779567+Matteoforni1@users.noreply.github.com>
2023-01-01 16:58:09 +0100

    Update processB.c

--- src/processB.c
+++ src/processB.c
@@ -1,4 +1,13 @@
 #include "./../include/processB_utilities.h"
+#include <stdio.h>
+#include <bmpfile.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 // we initialize stuff...
 int posx = 0;
@@ -10,18 +19,55 @@ const int SIZE = 960000;
 int i, fd_shm;
 char * ptr;
 
+// we declare bmp file, bmp_vec and pixel
+bmpfile_t *bmp;
+rgb_pixel_t pixelb = {255, 0, 0, 0};
+rgb_pixel_t pixelw = {0, 0, 0, 0};
+char bmp_vec[960000];
+
+// function for reading from shared memory
+void read_shm() {
+	int countn = 0;
+	sprintf(bmp_vec,"%s", (char *) ptr);
+	//printf("%s", bmp_vec);
+	for(int i = 0; i < SIZE; i++) {
+	if (bmp_vec[i] == 48) {
+		printf("%d\n", countn);
+		countn++;
+	}
+	}
+	printf("%d\n", countn);
+	
+}
+
+// main function
 int main(int argc, char const *argv[])
 {
-	    const char * shm_name = argv[1];
+	const char * shm_name = argv[1];
     	// Utility variable to avoid trigger resize event on launch
     	int first_resize = TRUE;
 
     	// Initialize UI
     	init_console_ui();
 
-	    mvaddch(LINES/2, COLS/2, '0');
+	mvaddch(LINES/2, COLS/2, '0');
         refresh();
+        posx = COLS/2;
+        posy = LINES/2;
 
+	// we open the shared memory
+	fd_shm = shm_open(shm_name, O_RDONLY, 0666);
+	if (fd_shm == 1) {
+		printf("Shared memory segment failed\n");
+		exit(1);
+	}
+
+	// we map the shared memory
+	ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, fd_shm, 0);
+	if (ptr == MAP_FAILED) {
+		printf("Map failed\n");
+	}
+	
     	// Infinite loop
     	while (TRUE) {
 
@@ -36,10 +82,12 @@ int main(int argc, char const *argv[])
             		else {
                 		reset_console_ui();
             		}
+            		mvaddch(LINES/2, COLS/2, '0');	// TO CANCEL AFTER DEBUGGING
+        		refresh();			// TO CANCEL AFTER DEBUGGING
         	}
 
         	else {
-            
+            		read_shm();
         	}
     	}
 
