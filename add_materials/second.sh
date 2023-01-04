2d4596c3442e14f95549fa1424d966abcd166509
Matteoforni1 <93779567+Matteoforni1@users.noreply.github.com>
2023-01-04 16:18:26 +0100

    Update second.sh

--- add_materials/second.sh
+++ add_materials/second.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 gcc src/master.c -o bin/master -lrt -lm
 gcc src/processA.c -lncurses -o bin/processA -lbmp -lrt -pthread -lm
-gcc src/processB.c -lncurses -o bin/processB -lrt -pthread -lm
+gcc src/processB.c -lncurses -o bin/processB -lbmp -lrt -pthread -lm
 ./bin/master
